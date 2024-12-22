0862319a42fcd292967aba789ff7bafe28139842
Islom Pardaboyev <ipardaboyev574@gmail.com>
2024-12-22 08:12:41 +0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { Button } from "./components/ui/button";
 import { calculatorContext } from "./utils";
 import { CHAT_ID, IP_API, TELEGRAM_TOKEN } from "./hook/useEnv";
